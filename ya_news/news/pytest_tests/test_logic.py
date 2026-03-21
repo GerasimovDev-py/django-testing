b0d0b15fa36d7f1053cdd688abf3ccbc6da0036e
Vladislav Gerasimov <projectmediastudio306@gmail.com>
2026-03-21 19:53:24 +0300

    Pep8

--- ya_news/news/pytest_tests/test_logic.py
+++ ya_news/news/pytest_tests/test_logic.py
@@ -10,8 +10,7 @@ class TestLogic:
     def test_anonymous_user_cant_send_comment(self, client, news):
         url = reverse('news:detail', args=(news.id,))
         comments_before = Comment.objects.count()
-        form_data = {'text': 'Текст комментария'}
-        response = client.post(url, data=form_data)
+        response = client.post(url, data={'text': 'Текст комментария'})
         login_url = reverse('login')
         assert response.status_code == 302
         assert response.url.startswith(login_url)
@@ -21,26 +20,19 @@ class TestLogic:
             self, author_client, news, author):
         url = reverse('news:detail', args=(news.id,))
         comments_before = Comment.objects.count()
-        form_data = {'text': 'Текст комментария'}
-        response = author_client.post(url, data=form_data)
+        response = author_client.post(url, data={'text': 'Текст комментария'})
         assert response.status_code == 302
         assert response.url == url
         assert Comment.objects.count() == comments_before + 1
         new_comment = Comment.objects.latest('id')
-        assert new_comment.text == form_data['text']
+        assert new_comment.text == 'Текст комментария'
         assert new_comment.news == news
         assert new_comment.author == author
 
     def test_comment_with_bad_words_not_published(self, author_client, news):
         url = reverse('news:detail', args=(news.id,))
         comments_before = Comment.objects.count()
-        response = author_client.post(url, data={'text': 'редиска'}, follow=True)
-        print(f"\nStatus: {response.status_code}")
-        print(f"Content: {response.content[:500]}")
-        if 'form' in response.context:
-            print(f"Form errors: {response.context['form'].errors}")
-        else:
-            print("No form in context")
+        response = author_client.post(url, data={'text': 'редиска'})
         assert Comment.objects.count() == comments_before
         assert response.status_code == 200
         assert 'form' in response.context
@@ -49,8 +41,7 @@ class TestLogic:
     def test_author_can_edit_comment(self, author_client, comment, news):
         url = reverse('news:edit', args=(comment.id,))
         new_text = 'Обновлённый текст комментария'
-        form_data = {'text': new_text}
-        response = author_client.post(url, data=form_data)
+        response = author_client.post(url, data={'text': new_text})
         assert response.status_code == 302
         assert response.url == reverse(
             'news:detail', args=(news.id,)
@@ -71,8 +62,9 @@ class TestLogic:
     def test_user_cant_edit_other_comment(self, not_author_client, comment):
         url = reverse('news:edit', args=(comment.id,))
         old_text = comment.text
-        form_data = {'text': 'Попытка изменить чужой комментарий'}
-        response = not_author_client.post(url, data=form_data)
+        response = not_author_client.post(
+            url, data={'text': 'Попытка изменить чужой комментарий'}
+        )
         assert response.status_code == 403
         comment.refresh_from_db()
         assert comment.text == old_text
