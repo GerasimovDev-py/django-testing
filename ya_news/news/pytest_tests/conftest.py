56cdf56e40bc4d341bb42ff8e242a41ac7750cea
Vladislav Gerasimov <projectmediastudio306@gmail.com>
2026-03-22 21:53:40 +0300

    f1

--- ya_news/news/pytest_tests/conftest.py
+++ ya_news/news/pytest_tests/conftest.py
@@ -1,37 +1,45 @@
+from datetime import date, timedelta
+
 import pytest
-from django.contrib.auth import get_user_model
+from django.conf import settings
+from django.test import Client
+from django.urls import reverse
 from django.utils import timezone
-from datetime import timedelta
 
-from news.models import News, Comment
-
-User = get_user_model()
+from news.models import Comment, News
 
 
 @pytest.fixture
-def author():
-    """Фикстура автора комментариев."""
-    return User.objects.create(username='Автор')
+def author(django_user_model):
+    return django_user_model.objects.create(username='Автор')
 
 
 @pytest.fixture
-def not_author():
-    """Фикстура пользователя, не являющегося автором."""
-    return User.objects.create(username='Не автор')
+def not_author(django_user_model):
+    return django_user_model.objects.create(username='Не автор')
 
 
 @pytest.fixture
-def news():
-    """Фикстура новости."""
-    return News.objects.create(
-        title='Заголовок новости',
-        text='Текст новости',
-    )
+def author_client(author):
+    client = Client()
+    client.force_login(author)
+    return client
+
+
+@pytest.fixture
+def not_author_client(not_author):
+    client = Client()
+    client.force_login(not_author)
+    return client
+
+
+@pytest.fixture
+def news(db):
+    return News.objects.create(title='Заголовок', text='Текст')
 
 
 @pytest.fixture
 def comment(author, news):
-    """Фикстура комментария."""
     return Comment.objects.create(
         news=news,
         author=author,
@@ -40,49 +48,65 @@ def comment(author, news):
 
 
 @pytest.fixture
-def bulk_news():
-    """Фикстура для создания 11 новостей с разными датами."""
-    from django.conf import settings
-    today = timezone.now().date()
+def pk_from_news(news):
+    return (news.pk,)
+
+
+@pytest.fixture
+def pk_from_news_in_list(news):
+    return (news.pk,)
+
+
+@pytest.fixture
+def pk_from_comment(comment):
+    return (comment.pk,)
+
+
+@pytest.fixture
+def home_url():
+    return reverse('news:home')
+
+
+@pytest.fixture
+def detail_url(pk_from_news):
+    return reverse('news:detail', args=pk_from_news)
+
+
+@pytest.fixture
+def edit_url(pk_from_comment):
+    return reverse('news:edit', args=pk_from_comment)
+
+
+@pytest.fixture
+def delete_url(pk_from_comment):
+    return reverse('news:delete', args=pk_from_comment)
+
+
+@pytest.fixture
+def bulk_news(db):
+    """Создает список новостей с разными датами для теста пагинации и веса."""
+    today = date.today()
     all_news = [
         News(
             title=f'Новость {index}',
-            text='Просто текст.',
+            text='Текст',
             date=today - timedelta(days=index)
         )
         for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
     ]
     News.objects.bulk_create(all_news)
-    return News.objects.all()
 
 
 @pytest.fixture
-def comment_with_different_dates(news, author):
-    """Фикстура для создания комментариев с разными датами."""
+def comment_with_different_dates(author, news):
+    """Создает комментарии с разным временем создания."""
     now = timezone.now()
-    for index in range(10):
+    for index in range(2):
         comment = Comment.objects.create(
             news=news,
             author=author,
-            text=f'Комментарий {index}'
+            text=f'Текст {index}',
         )
         comment.created = now + timedelta(days=index)
         comment.save()
-
-
-@pytest.fixture
-def author_client(author):
-    """Клиент с авторизацией автора."""
-    from django.test.client import Client
-    client = Client()
-    client.force_login(author)
-    return client
-
-
-@pytest.fixture
-def not_author_client(not_author):
-    """Клиент с авторизацией пользователя, не являющегося автором."""
-    from django.test.client import Client
-    client = Client()
-    client.force_login(not_author)
-    return client
+    return comment
--- ya_news/news/pytest_tests/test_content.py
+++ ya_news/news/pytest_tests/test_content.py
@@ -3,16 +3,16 @@ from django.conf import settings
 
 from news.forms import CommentForm
 
-pytestmark = pytest.mark.django_db
 
-
+@pytest.mark.django_db
 def test_news_count(client, bulk_news, home_url):
     response = client.get(home_url)
     object_list = response.context['object_list']
-    news_count = object_list.count()
+    news_count = len(object_list)
     assert news_count == settings.NEWS_COUNT_ON_HOME_PAGE
 
 
+@pytest.mark.django_db
 def test_news_order(client, bulk_news, home_url):
     response = client.get(home_url)
     object_list = response.context['object_list']
@@ -21,22 +21,24 @@ def test_news_order(client, bulk_news, home_url):
     assert all_dates == sorted_dates
 
 
-def test_comments_order(
-        client, news, comment_with_different_dates, detail_url):
+@pytest.mark.django_db
+def test_comments_order(client, detail_url, comment_with_different_dates):
     response = client.get(detail_url)
     assert 'news' in response.context
-    news_from_context = response.context['news']
-    all_comments = news_from_context.comments.all()
+    news = response.context['news']
+    all_comments = news.comments.all()
     all_timestamps = [comment.created for comment in all_comments]
     sorted_timestamps = sorted(all_timestamps)
     assert all_timestamps == sorted_timestamps
 
 
+@pytest.mark.django_db
 def test_anonymous_client_has_no_form(client, detail_url):
     response = client.get(detail_url)
     assert 'form' not in response.context
 
 
+@pytest.mark.django_db
 def test_authorized_client_has_form(author_client, detail_url):
     response = author_client.get(detail_url)
     assert 'form' in response.context
